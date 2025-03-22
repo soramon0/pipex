09bfc3d13841d7a13cd6c8bfcc510005c336b4dd
soramon0 <23435312+soramon0@users.noreply.github.com>
2025-03-22 23:59:22 +0000

    feat: search bin in path (WIP)

--- src/cmd.c
+++ src/cmd.c
@@ -12,24 +12,58 @@
 
 #include "pipex.h"
 
-int	run_cmd(char *pathname, char *argv[], char *envp[])
+char *strs_search(char *haystack[], char *needle)
 {
-	if (access(pathname, F_OK) == -1)
+	int	i;
+	int	n;
+
+	if (haystack == NULL || needle == NULL)
+		return (NULL);
+	n = ft_strlen(needle);
+	i = 0;
+	while (haystack[i] != NULL)
+	{
+		if (ft_strncmp(haystack[i], needle, n) == 0)
+			return (haystack[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+char *get_binpath(char *filename, char *path)
+{
+	if (path == NULL || filename == NULL)
+		return (NULL);
+	ft_printf_fd(2, path);
+	return (NULL);
+}
+
+int	run_cmd(char *bin, char *argv[], char *envp[])
+{
+	if (access(bin, F_OK) == -1)
 		return (127);
-	if (access(pathname, X_OK) == -1)
+	if (access(bin, X_OK) == -1)
 		return (126);
-	return (execve(pathname, argv, envp));
+	return (execve(bin, argv, envp));
 }
 
 int	exec_cmd(char *cmd, char *envp[])
 {
 	char	**cmd_argv;
 	int		status;
+	char	*path;
+	char	*bin;
 
+	path = strs_search(envp, "PATHS");
+	if (path == NULL)
+		return (ft_printf_fd(2, "PATH undefined\n"), EXIT_FAILURE);
 	cmd_argv = ft_split(cmd, ' ');
 	if (cmd_argv == NULL || cmd_argv[0] == NULL)
 		return (ft_split_free(cmd_argv), EXIT_FAILURE);
-	status = run_cmd(cmd_argv[0], cmd_argv, envp);
+	bin = get_binpath(cmd_argv[0], path);
+	if (bin == NULL)
+		return (ft_split_free(cmd_argv), EXIT_FAILURE);
+	status = run_cmd(bin, cmd_argv, envp);
 	if (status != 0)
 	{
 		perror(cmd_argv[0]);
