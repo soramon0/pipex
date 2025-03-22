a27b0d6960f6cac897ba6c0557ddf8df470dfcb8
soramon0 <23435312+soramon0@users.noreply.github.com>
2025-03-22 23:32:27 +0000

    feat: return access errors

--- src/cmd.c
+++ src/cmd.c
@@ -14,21 +14,26 @@
 
 int	run_cmd(char *pathname, char *argv[], char *envp[])
 {
+	if (access(pathname, F_OK) == -1)
+		return (127);
+	if (access(pathname, X_OK) == -1)
+		return (126);
 	return (execve(pathname, argv, envp));
 }
 
 int	exec_cmd(char *cmd, char *envp[])
 {
 	char	**cmd_argv;
+	int		status;
 
 	cmd_argv = ft_split(cmd, ' ');
 	if (cmd_argv == NULL || cmd_argv[0] == NULL)
-		return (EXIT_FAILURE);
-	if (run_cmd(cmd_argv[0], cmd_argv, envp) == -1)
+		return (ft_split_free(cmd_argv), EXIT_FAILURE);
+	status = run_cmd(cmd_argv[0], cmd_argv, envp);
+	if (status != 0)
 	{
 		perror(cmd_argv[0]);
 		ft_split_free(cmd_argv);
-		return (EXIT_FAILURE);
 	}
-	return (EXIT_SUCCESS);
+	return (status);
 }
